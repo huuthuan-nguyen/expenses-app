3ec477de145e024190742979c930b763073a4a2a
Thuận Nguyễn <kean@iMac-Pro.local>
2021-06-08 20:53:11 +0700

    listtile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -16,7 +16,9 @@ class TransactionList extends StatelessWidget {
                   "No transactions added yet!",
                   style: Theme.of(context).textTheme.headline6,
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Container(
                   height: 200,
                   child: Image.asset(
@@ -28,41 +30,63 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (BuildContext context, int index) {
+                // return Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin:
+                //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(
+                //             border: Border.all(
+                //           color: Theme.of(context).primaryColor,
+                //           width: 2,
+                //         )),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           "\$${transactions[index].amount.toStringAsFixed(2)}",
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Text(
+                //             transactions[index].title,
+                //             style: Theme.of(context).textTheme.headline6,
+                //           ),
+                //           Text(
+                //               DateFormat.yMMMd()
+                //                   .format(transactions[index].date),
+                //               style: TextStyle(color: Colors.grey))
+                //         ],
+                //       ),
+                //     ],
+                //   ),
+                // );
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "\$${transactions[index].amount.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Text('\$${transactions[index].amount}'),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                              DateFormat.yMMMd()
-                                  .format(transactions[index].date),
-                              style: TextStyle(color: Colors.grey))
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
               },
